1d3306ff142d260b539b1f6069c0693b44a2c6e5
Ben-hur Santos Ott <benhur.ott@gmail.com>
2016-10-01 21:39:09 -0300

    base code

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,6 +2,29 @@ angular.module('dojo', [])
 
 .controller('HomeCtrl', ['$scope', function($scope){
 
-    $scope.title = 'DOJO!';
+    $scope.movies = [
+        {
+            id: 1,
+            title: 'The Walking Dead',
+            ranking: 4,
+            imageUrl: 'images/the-walking-dead.png'
+        },
+        {
+            id: 2,
+            title: 'Stranger Things',
+            ranking: 5,
+            imageUrl: 'images/stranger-things.jpg'
+        },
+        {
+            id: 3,
+            title: 'American Horror Story',
+            ranking: 3,
+            imageUrl: 'images/american-horror-story.jpg'
+        }
+    ];
+
+    $scope.getStars = function(number) {
+        return new Array(number);
+    };
 
 }]);
