c276369ad72be0404682b3714b09f894894c029b
Ben-hur Santos Ott <benhur.ott@gmail.com>
2016-10-02 23:10:56 -0300

    complete movie-collection-carousel

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,65 +1,78 @@
 angular.module('dojo', [])
 
-.controller('HomeCtrl', ['$scope', function($scope){
+    .controller('HomeCtrl', ['$scope', function ($scope) {
 
-    $scope.movies = [
-        {
-            id: 1,
-            title: 'The Walking Dead',
-            ranking: 4,
-            imageUrl: 'images/the-walking-dead.png'
-        },
-        {
-            id: 2,
-            title: 'Stranger Things',
-            ranking: 5,
-            imageUrl: 'images/stranger-things.jpg'
-        },
-        {
-            id: 3,
-            title: 'American Horror Story',
-            ranking: 3,
-            imageUrl: 'images/american-horror-story.jpg'
-        }
-    ];
+        $scope.movies = [
+            {
+                id: 1,
+                title: 'The Walking Dead',
+                ranking: 4,
+                imageUrl: 'images/the-walking-dead.png'
+            },
+            {
+                id: 2,
+                title: 'Stranger Things',
+                ranking: 5,
+                imageUrl: 'images/stranger-things.jpg'
+            },
+            {
+                id: 3,
+                title: 'American Horror Story',
+                ranking: 3,
+                imageUrl: 'images/american-horror-story.jpg'
+            }
+        ];
 
-    $scope.onSelectMovie = function(movie) {
-        alert(movie.title);
-    };
-
-}])
+        $scope.onSelectMovie = function (movie) {
+            alert(movie.title);
+        };
 
 
-.directive('myStars', [function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/directives/my-stars/my-stars.html',
-        scope: {
-            stars: '='
-        },
-        link: function(scope, element, attrs) {
-            scope.getStars = function(number) {
-                return new Array(number);
-            };
-        }
-    };
-}])
+        $(document).ready(function(){
+            $('.movie-collection').slick();
+        });
 
-.directive('movieCollection', [function() {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/directives/movie-collection/movie-collection.html',
-        scope: {
-            movies: '=',
-            onSelectMovie: '&'
-        },
-        link: function(scope, element, attrs) {
-            scope.__onSelectMovie = function(movie) {
-                scope.onSelectMovie()(movie);
-            };
-        }
-    };
-}])
+    }])
+
+
+    .directive('myStars', [function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/my-stars/my-stars.html',
+            scope: {
+                stars: '='
+            },
+            link: function (scope, element, attrs) {
+                scope.getStars = function (number) {
+                    return new Array(number);
+                };
+            }
+        };
+    }])
+
+    .directive('movieCollection', [function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'templates/directives/movie-collection/movie-collection.html',
+            scope: {
+                movies: '=',
+                onSelectMovie: '&'
+            },
+            link: function (scope, element, attrs) {
+                scope.__onSelectMovie = function (movie) {
+                    scope.onSelectMovie()(movie);
+                };
+
+                angular.element(document).ready(function () {
+                    $('.movie-collection').slick({
+                        dots: true,
+                        autoplay: true,
+                        autoplaySpeed: 3000,
+                    });;
+                });
+            }
+        };
+    }])
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,27 @@ router.get('/', function (req, res, next) {
     });
 });
 
+router.get('/movies', function(req, res, next) {
+    res.json([
+        {
+            id: 1,
+            title: 'The Walking Dead',
+            ranking: 4,
+            imageUrl: 'images/the-walking-dead.png'
+        },
+        {
+            id: 2,
+            title: 'Stranger Things',
+            ranking: 5,
+            imageUrl: 'images/stranger-things.jpg'
+        },
+        {
+            id: 3,
+            title: 'American Horror Story',
+            ranking: 3,
+            imageUrl: 'images/american-horror-story.jpg'
+        }
+    ]);
+})
+
 module.exports = router;
